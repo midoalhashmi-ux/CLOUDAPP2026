969c815a3df90c3ad9a38cc6c70e26ba3aa82cc8
midoalhashmi-ux <midoalhashmi@gmail.com>
2026-08-24 12:23:20 +0300

    Add files via upload

---
+++ lib/core/services/content_service.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/category_model.dart';
+import '../models/channel_model.dart';
+
+class ContentService {
+  static Stream<List<CategoryModel>> watchCategories() {
+    return FirebaseFirestore.instance
+        .collection('categories')
+        .orderBy('order')
+        .snapshots()
+        .map((snap) => snap.docs
+            .map((d) => CategoryModel.fromMap(d.id, d.data()))
+            .toList());
+  }
+
+  static Stream<List<ChannelModel>> watchChannelsForCategory(
+      String categoryId) {
+    return FirebaseFirestore.instance
+        .collection('channels')
+        .where('categoryId', isEqualTo: categoryId)
+        .snapshots()
+        .map((snap) => snap.docs
+            .map((d) => ChannelModel.fromMap(d.id, d.data()))
+            .toList());
+  }
+}
